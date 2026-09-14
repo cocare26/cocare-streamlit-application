15b9221026da32499c33e841661807263ac8f117
cocare26 <co.care26@gmail.com>
2026-09-15 02:31:42 +0300

    Delete utils directory

--- utils/notification_utils (1).py
+++
@@ -1,8 +0,0 @@
-def send_notification(notification_type: str, payload: dict):
-    if notification_type == "none":
-        return False
-
-    print(f"[NOTIFICATION] type={notification_type}")
-    print(f"[PAYLOAD] {payload}")
-
-    return True
