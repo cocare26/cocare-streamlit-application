4defd90debb4b66eb4db9b6b1bad2f0e34b4ec0d
cocare26 <co.care26@gmail.com>
2026-09-15 02:44:25 +0300

    Update language_guard.py

--- language_guard.py
+++ language_guard.py
@@ -1,28 +1,25 @@
 import streamlit as st
 
 AR_TO_EN = {
-
     "settingar.py": "pages/Settings.py",
     "Change_Languagear.py": "pages/ChangeLanguage.py",
     "Change_Passwordar.py": "pages/ChangePassword.py",
     "ContactUsar.py": "pages/ContactUs.py",
     "Rate_Appar.py": "pages/RateApp.py",
     "Report_Problemar.py": "pages/ReportProblem.py",
-    "Customer_ar.py": "pages/2_Customer.py",
-
+    "Customer_ar.py": "pages/2_Customer_EN.py",
 }
 
 EN_TO_AR = {
-
     "Settings.py": "pages/settingar.py",
     "ChangeLanguage.py": "pages/Change_Languagear.py",
     "ChangePassword.py": "pages/Change_Passwordar.py",
     "ContactUs.py": "pages/ContactUsar.py",
     "RateApp.py": "pages/Rate_Appar.py",
     "ReportProblem.py": "pages/Report_Problemar.py",
-    "2_Customer.py": "pages/Customer_ar.py",
-
+    "2_Customer_EN.py": "pages/Customer_ar.py",
 }
+
 
 def check_language(current_file):
 
@@ -31,16 +28,8 @@ def check_language(current_file):
 
     lang = st.session_state.lang
 
-    # DEBUG
-    st.write("FILE =", current_file)
-    st.write("LANG =", lang)
+    if lang == "ar" and current_file in EN_TO_AR:
+        st.switch_page(EN_TO_AR[current_file])
 
-    if lang == "ar":
-
-        if current_file in EN_TO_AR:
-            st.switch_page(EN_TO_AR[current_file])
-
-    elif lang == "en":
-
-        if current_file in AR_TO_EN:
-            st.switch_page(AR_TO_EN[current_file])
+    elif lang == "en" and current_file in AR_TO_EN:
+        st.switch_page(AR_TO_EN[current_file])
